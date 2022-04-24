eb8845d5e695151492c78f929f97937785c053c6
Christoph M. Becker <cmbecker69@gmx.de>
2022-04-24 15:26:10 +0200

    Switch to GNU style file headers

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
 
 /**
- * Front-end of Schedule_XH.
+ * Copyright 2012-2022 Christoph M. Becker
  *
- * PHP versions 4 and 5
+ * This file is part of Schedule_XH.
  *
- * @category  CMSimple_XH
- * @package   Schedule
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Schedule_XH
+ * Schedule_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Schedule_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Schedule_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*

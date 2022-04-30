731059aa776591b4387fe7475824b1b327d606da
Christoph M. Becker <cmbecker69@gmx.de>
2022-04-30 15:47:31 +0200

    Re-indent planner template

--- views/planner.php
+++ views/planner.php
@@ -4,41 +4,41 @@
 <?php else:?>
 <div class="schedule">
 <?php endif?>
-    <table class="schedule">
-        <thead>
-            <tr>
-                <th></th>
+  <table class="schedule">
+    <thead>
+      <tr>
+        <th></th>
 <?php foreach ($options as $option):?>
-                <th><?=$option?></th>
+        <th><?=$option?></th>
 <?php endforeach?>
-            </tr>
-        </thead>
-        <tbody>
+      </tr>
+    </thead>
+    <tbody>
 <?php foreach ($users as $user => $votes):?>
-            <tr>
-                <td class="schedule_user"><?=$user?></td>
-<?php foreach ($votes as $option => $voted):?>
-                <td class="schedule_<?=$voted ? 'green' : 'red'?>"><?=$cells[$user][$option]?></td>
-<?php endforeach?>
-            </tr>
+      <tr>
+        <td class="schedule_user"><?=$user?></td>
+<?php   foreach ($votes as $option => $voted):?>
+        <td class="schedule_<?=$voted ? 'green' : 'red'?>"><?=$cells[$user][$option]?></td>
+<?php   endforeach?>
+      </tr>
 <?php endforeach?>
 
 <?php if ($showTotals):?>
-            <tr class="schedule_total">
-                <td class="schedule_user"><?=$ptx['total']?></td>
-<?php foreach ($counts as $count):?>
-                <td><?=$count?></td>
-<?php endforeach?>
-            </tr>
+      <tr class="schedule_total">
+        <td class="schedule_user"><?=$ptx['total']?></td>
+<?php   foreach ($counts as $count):?>
+        <td><?=$count?></td>
+<?php   endforeach?>
+      </tr>
 <?php endif?>
 
 <?php if ($currentUser):?>
-            <tr class="schedule_buttons">
-                <td colspan="<?=count($options) + 1?>"><?=$submit?></td>
-            </tr>
+      <tr class="schedule_buttons">
+        <td colspan="<?=count($options) + 1?>"><?=$submit?></td>
+      </tr>
 <?php endif?>
-        </tbody>
-    </table>
+    </tbody>
+  </table>
 <?php if ($currentUser):?>
 </form>
 <?php else:?>

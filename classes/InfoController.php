038df26cf61419b8415ee1f7c316869f9dc3628e
Christoph M. Becker <cmbecker69@gmx.de>
2025-03-28 13:17:14 +0100

    Drop superfluous system check for ext/session
    This is already required and checked by CMSimple_XH.

--- classes/InfoController.php
+++ classes/InfoController.php
@@ -72,13 +72,6 @@ final class InfoController
             "arg" => $phpVersion,
             "class" => $this->systemChecker->checkVersion(PHP_VERSION, $phpVersion) >= 0 ? "xh_success" : "xh_fail",
         ];
-        foreach (['session'] as $ext) {
-            $checks[] = [
-                "key" => "syscheck_extension",
-                "arg" => $ext,
-                "class" => $this->systemChecker->checkExtension($ext) ? "xh_success" : "xh_fail",
-            ];
-        }
         foreach (['config/', 'css/', 'languages/'] as $folder) {
             $folders[] = $this->pluginFolder . $folder;
         }
